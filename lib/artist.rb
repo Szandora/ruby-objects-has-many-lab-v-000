f4b3a94af4d0dbfc62124de7e7006ae40ab3f1f6
Sheila Wittman <szandora789@gmail.com>
2018-06-13 21:15:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,23 @@
-class Artist
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    song
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    song
-  end
-
-  def songs
-    @songs
-  end
-
-  def self.song_count
-    Song.all.size
-  end
-
-end
++class Artist
++  attr_accessor :name, :songs
++
++  def initialize(name)
++    @songs = []
++    @name = name
++  end
++
++  def add_song(song)
++    song.artist = self
++    @songs << song
++  end
++
++  def add_song_by_name(title)
++    song = Song.new(title)
++    song.artist = self
++    @songs << song
++  end
++
++  def self.song_count
++    Song.songs.length
++  end
++end
